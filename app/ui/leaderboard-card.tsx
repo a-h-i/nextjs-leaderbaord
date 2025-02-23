43ad1d3087ee2050eb2e15f8c29f7b9df81c1e32
Ahmed Ismail <ahm3d.hisham@gmail.com>
2025-02-23 15:06:14 +0000

    truncation for leaderboard address

--- app/ui/leaderboard-card.tsx
+++ app/ui/leaderboard-card.tsx
@@ -15,18 +15,18 @@ export default function LeaderboardCard(props: {
 
         <Link  href={`/user/${props.entry.username}?rank=${props.entry.rank}&address=${props.entry.address}&level=${props.entry.level}&xp=${props.entry.xp}&avatarCid=${props.entry.avatarCid}`}
             className={`block my-4 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${styles.leaderboardWrapper}`}>
-            <header className="flex flex-row items-center ">
+            <header className="flex flex-row items-center gap-1 ">
                 <Avatar
                     img={`https://ipfs.io/ipfs/${props.entry.avatarCid}`}
                     alt={props.entry.username}
                     rounded
                 />
-                <h5 className="mx-3.5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.entry.username}</h5>
+                <h5 className="sm:mx-1 sm:text-lg md:mx-3.5 md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.entry.username}</h5>
             </header>
-            <ul className="font-normal text-gray-700 dark:text-gray-400">
-                <li><span className="font-bold">level: </span>{props.entry.level}</li>
-                <li><span className="font-bold">xp: </span>${props.entry.xp}</li>
-                <li><span className="font-bold">address: </span>{props.entry.address}</li>
+            <ul className="font-normal text-gray-700 dark:text-gray-400 flex flex-col gap-1">
+                <li ><span className="font-bold">level: </span>{props.entry.level}</li>
+                <li ><span className="font-bold">xp: </span>${props.entry.xp}</li>
+                <li className="overflow-ellipsis overflow-hidden whitespace-nowrap w-1/2 md:w-full"><span className="font-bold">address: </span>{props.entry.address}</li>
             </ul>
         </Link>
 
